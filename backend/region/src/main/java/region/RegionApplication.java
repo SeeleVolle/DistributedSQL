050ca16f7a4842bf0688c19343ecac65022c4d4b
squarehuang <jjhuang5@outlook.com>
2024-05-09 21:38:15 +0800

    Finish RegionServer v1.0

--- backend/region/src/main/java/region/RegionApplication.java
+++ backend/region/src/main/java/region/RegionApplication.java
@@ -25,6 +25,7 @@ import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.nio.charset.StandardCharsets;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -60,14 +61,17 @@ public class RegionApplication {
         try{
             //获取本机地址：ip:port
             String ip = InetAddress.getLocalHost().getHostAddress();
+            System.out.println("本机IP地址：" + ip);
             String localaddr = ip+":"+port;
-            //获取数据库连接
+//            获取数据库连接
             String url = "jdbc:mysql://"+ ip + ":3306/DISTRIBUTED";
             databaseConnection = new DatabaseConnection(url, username, password);
+            databaseConnection.connect();
             //初始化zookeeper
             zookeeper = new Zookeeper(localaddr, zkServerAddr, databaseConnection, maxRegions, maxServers);
             zookeeper.connect();
         } catch (Exception e){
+            e.printStackTrace();
             System.out.println("Error: Region Server init failed.");
         }
     }
@@ -75,17 +79,28 @@ public class RegionApplication {
     @PreDestroy
     void destroy(){
         try{
+            System.out.println("Region Server close.");
             zookeeper.close();
         } catch (Exception e){
             System.out.println("Error: Region Server close failed.");
         }
     }
+
+    @RequestMapping("/test")
+    public String test(@RequestBody String sql){
+        System.out.println("SQL: " + sql);
+        return "Region Server is running.";
+    }
+
+
+
     @RequestMapping("/create")
-    public JSONObject createTable(@RequestBody String sql, @RequestBody String tableName){
+    public JSONObject createTable(@RequestBody SQLParams params){
+        System.out.println("SQL: " + params.getSql());
         JSONObject res = new JSONObject();
         //1. 执行SQL语句
         try{
-            executeSQLUpdated(sql);
+            executeSQLUpdated(params.getSql());
         }catch (Exception e){
             System.out.println("Error: Region Server create table failed.");
             res.put("status", "500");
@@ -95,10 +110,10 @@ public class RegionApplication {
         //2. master转发sql语句到该Region下的所有slave
         if(zookeeper.isMaster()){
             while(!zookeeper.isReady());
-            forward(sql, "create", tableName);
+            forward(params.getSql(), "create", params.getTableName());
         //3. 更新zk下的table信息
             try{
-                zookeeper.addTable(tableName);
+                zookeeper.addTable(params.getTableName());
             }catch (Exception e){
                 System.out.println("Error: Region Server update table info failed.");
                 res.put("status", "500");
@@ -112,11 +127,13 @@ public class RegionApplication {
     }
 
     @RequestMapping("/drop")
-    public JSONObject dropTable(@RequestBody String sql, @RequestBody String tableName){
+    public JSONObject dropTable(@RequestBody SQLParams params){
+        System.out.println("SQL: " + params.getSql());
+
         JSONObject res = new JSONObject();
         //1. 执行SQL语句
         try{
-            executeSQLUpdated(sql);
+            executeSQLUpdated(params.getSql());
         }catch (Exception e){
             System.out.println("Error: Region Server drop table failed.");
             res.put("status", "500");
@@ -126,10 +143,10 @@ public class RegionApplication {
         //2. master转发sql语句到该Region下的所有slave
         if(zookeeper.isMaster()){
             while(!zookeeper.isReady());
-            forward(sql, "drop", tableName);
+            forward(params.getSql(), "drop", params.getTableName());
         //3. 更新zk下的table信息
             try{
-                zookeeper.removeTable(tableName);
+                zookeeper.removeTable(params.getTableName());
             }catch (Exception e){
                 System.out.println("Error: Region Server update table info failed.");
                 res.put("status", "500");
@@ -143,17 +160,31 @@ public class RegionApplication {
     }
 
     @RequestMapping("/query")
-    public JSONObject queryTable(@RequestBody String sql, @RequestBody String tableName) {
+    public JSONObject queryTable(@RequestBody SQLParams params) {
+        System.out.println("SQL: " + params.getSql());
+
         JSONObject res = new JSONObject();
         //1. 在本slave执行SQL语句
         try{
-            ResultSet rs = executeSQLQuery(sql);
+            ResultSet rs = executeSQLQuery(params.getSql());
+            ResultSetMetaData meta = rs.getMetaData();
+
+            List<Object[]> datalist = new ArrayList<>();
+            while(rs.next()){
+                Object[] rowData = new Object[rs.getMetaData().getColumnCount()];
+                for(int i = 0; i < rs.getMetaData().getColumnCount(); i++){
+                    rowData[i] = rs.getObject(i+1);
+                }
+                datalist.add(rowData);
+            }
+
+            CheckSum checkSum = new CheckSum(databaseConnection);
+            String myCRCResult = String.valueOf(checkSum.getCRC4Result(datalist));
         //2. 转发sql语句到其他slave进行vote表决
-            if(vote(sql, tableName, rs)){
+            if(vote(params.getSql(), params.getTableName(), myCRCResult)){
                 res.put("status", "200");
                 res.put("msg", "Query table successfully");
         //3. 返回查询结果
-                ResultSetMetaData meta = rs.getMetaData();
                 int columns = meta.getColumnCount();
                 StringBuilder first_row = new StringBuilder();
                 for(int i = 1; i <= columns; i++){
@@ -163,23 +194,22 @@ public class RegionApplication {
                         first_row.append(meta.getColumnName(i)).append(" ");
                 }
                 res.put("Column Name:", first_row.toString());
-                int rowCount = 1;
-                while(rs.next()){
+                for(int i = 0; i < datalist.size(); i++){
                     StringBuilder row = new StringBuilder();
-                    for(int i = 1; i <= columns; i++){
-                        if(i == columns)
-                            row.append(rs.getString(i));
+                    for(int j = 0; j < datalist.get(0).length; j++){
+                        if(j == datalist.get(0).length - 1)
+                            row.append(datalist.get(i)[j]);
                         else
-                            row.append(rs.getString(i)).append(" ");
+                            row.append(datalist.get(i)[j]).append(" ");
                     }
-                    res.put("Row "+ rowCount +" :", row.toString());
-                    rowCount++;
+                    res.put("Row "+ i+1 +" :", row.toString());
                 }
             }
             else
                 throw new Exception();
 
         }catch (Exception e){
+            e.printStackTrace();
             System.out.println("Error: Region Server query table failed.");
             res.put("status", "500");
             res.put("message", "Query table failed.");
@@ -188,11 +218,13 @@ public class RegionApplication {
     }
 
     @RequestMapping("/update")
-    public JSONObject updateTable(@RequestBody String sql, @RequestBody String tableName){
+    public JSONObject updateTable(@RequestBody SQLParams params){
+        System.out.println("SQL: " + params.getSql());
+
         JSONObject res = new JSONObject();
         //1. 执行SQL语句
         try{
-            executeSQLUpdated(sql);
+            executeSQLUpdated(params.getSql());
         }catch (Exception e){
             System.out.println("Error: Region Server update table failed.");
             res.put("status", "500");
@@ -202,7 +234,7 @@ public class RegionApplication {
         //2. master转发sql语句到该Region下的所有slave
         if(zookeeper.isMaster()){
             while(!zookeeper.isReady());
-            forward(sql, "update", tableName);
+            forward(params.getSql(), "update", params.getTableName());
         }
         res.put("status", "200");
         res.put("msg", "Update table successfully");
@@ -217,6 +249,7 @@ public class RegionApplication {
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.executeUpdate();
         }catch (Exception e){
+            e.printStackTrace();
             System.out.println("Error: Region Server execute sql failed.");
         }
     }
@@ -230,6 +263,7 @@ public class RegionApplication {
             ResultSet rs = ps.executeQuery();
             return rs;
         }catch (Exception e){
+            e.printStackTrace();
             System.out.println("Error: Region Server execute sql failed.");
         }
         return null;
@@ -259,10 +293,8 @@ public class RegionApplication {
         }
     }
 
-    public boolean vote(String sql, String tableName, ResultSet myrs) throws SQLException {
+    public boolean vote(String sql, String tableName, String myCRCResult) throws SQLException {
         List<String> slavesAddrs = zookeeper.getSlaves();
-        CheckSum checkSum = new CheckSum(databaseConnection);
-        String myCRCResult = String.valueOf(checkSum.getCRC4Result(myrs));
         int supports_num = 1;
         //转发请求并获取结果
         for(String slaveAddr: slavesAddrs) {
@@ -290,12 +322,12 @@ public class RegionApplication {
     }
 
     @RequestMapping("/votequery")
-    public JSONObject  votequery(String sql, String tableName){
+    public JSONObject  votequery(@RequestBody SQLParams params){
         JSONObject res = new JSONObject();
-        ResultSet rs = executeSQLQuery(sql);
+        ResultSet rs = executeSQLQuery(params.getSql());
         CheckSum checkSum = new CheckSum(databaseConnection);
         try{
-            res.put("CRCResult", checkSum.getCRC4Result(rs));
+            res.put("CRCResult", checkSum.getCRC4ResultSet(rs));
         }catch(Exception e){
             res.put("status",  "500");
             System.out.println("Error: Get CRC4Result failed.");
@@ -304,6 +336,31 @@ public class RegionApplication {
         res.put("status", "200");
         return res;
     }
-
-
 }
+
+class SQLParams{
+    private String sql;
+    private String tableName;
+
+    SQLParams(String sql, String tableName){
+        this.sql = sql;
+        this.tableName = tableName;
+    }
+
+    public String getSql() {
+        return sql;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setSql(String sql) {
+        this.sql = sql;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+}
+
